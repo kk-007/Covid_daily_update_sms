e51318113b2a8c964c7f7cbf5cee914c650f5876
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-06-11 16:05:22 +0530

    added mail

--- app.js
+++ app.js
@@ -1,46 +1,85 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-let input_District = [
-    {
-        state:'Gujarat',
-        district:'Surat'
-    },
-    {
-        state:'Maharashtra',
-        district:'Mumbai'
-    }
-];
-let input_numbers = [
-    9924011385
+// let input_District = [
+//     {
+//         state:'Gujarat',
+//         district:'Surat'
+//     },
+//     {
+//         state:'Maharashtra',
+//         district:'Mumbai'
+//     }
+// ];
+let input_email = [
+    'kevalnavadiya39@gmail.com',
+    'bhutbhutani39@gmail.com'
 ]
 
-setInterval(()=>{
-    axios.get('https://api.covid19india.org/districts_daily.json')
-  .then(function (response) {
-    let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
-    let today = new Date().toISOString().slice(0,10);
-    let res={};
+// let flag = true;
 
-    input_District.forEach(e => {
-        let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
-        delete data[0].date;
-        delete data[1].date;
-        if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
-            res.active = data[1].active-data[0].active;
-            res.confirmed = data[1].confirmed-data[0].confirmed;
-            res.deceased = data[1].deceased-data[0].deceased;
-            res.recovered = data[1].recovered-data[0].recovered;
-            sendMessage(res);
-        }else{
-            console.log('same');
-        }
-    });
-  })
-  .catch(function (error) {
-    // handle error
+// setInterval(()=>{
+//     axios.get('https://api.covid19india.org/districts_daily.json')
+//   .then(function (response) {
+//     let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
+//     let today = new Date().toISOString().slice(0,10);
+//     let res={};
+
+//     input_District.forEach(e => {
+//         let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
+//         delete data[0].date;
+//         delete data[1].date;
+//         if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
+//             if(flag){
+//                 res.active = data[1].active-data[0].active;
+//                 res.confirmed = data[1].confirmed-data[0].confirmed;
+//                 res.deceased = data[1].deceased-data[0].deceased;
+//                 res.recovered = data[1].recovered-data[0].recovered;
+//                 flag=false;
+//                 sendMessage(res);
+//             }
+//         }else{
+//             flag=true;
+//             console.log('same');
+//         }
+//     });
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
+// },5000);
+
+
+
+
+
+
+
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+var mailOptions = {
+  from: process.env.EMAIL,
+  to: input_email.join(','),
+  subject: 'Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
     console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-},60000*5);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
