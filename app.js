e38ae29df4de9c8f05445cc90a05dbcfa7292d33
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-06-11 16:11:19 +0530

    final

--- app.js
+++ app.js
@@ -1,85 +1,77 @@
-// const axios = require('axios');
+const axios = require('axios');
+var nodemailer = require('nodemailer');
+require('dotenv').config();
 
-// let input_District = [
-//     {
-//         state:'Gujarat',
-//         district:'Surat'
-//     },
-//     {
-//         state:'Maharashtra',
-//         district:'Mumbai'
-//     }
-// ];
+let input_District = [
+    {
+        state:'Gujarat',
+        district:'Surat'
+    },
+    {
+        state:'Maharashtra',
+        district:'Mumbai'
+    }
+];
+
 let input_email = [
     'kevalnavadiya39@gmail.com',
     'bhutbhutani39@gmail.com'
 ]
 
-// let flag = true;
+let flag = true;
 
-// setInterval(()=>{
-//     axios.get('https://api.covid19india.org/districts_daily.json')
-//   .then(function (response) {
-//     let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
-//     let today = new Date().toISOString().slice(0,10);
-//     let res={};
+setInterval(()=>{
+    axios.get('https://api.covid19india.org/districts_daily.json')
+  .then(function (response) {
+    let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
+    let today = new Date().toISOString().slice(0,10);
+    let res={};
 
-//     input_District.forEach(e => {
-//         let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
-//         delete data[0].date;
-//         delete data[1].date;
-//         if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
-//             if(flag){
-//                 res.active = data[1].active-data[0].active;
-//                 res.confirmed = data[1].confirmed-data[0].confirmed;
-//                 res.deceased = data[1].deceased-data[0].deceased;
-//                 res.recovered = data[1].recovered-data[0].recovered;
-//                 flag=false;
-//                 sendMessage(res);
-//             }
-//         }else{
-//             flag=true;
-//             console.log('same');
-//         }
-//     });
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-// },5000);
-
-
-
-
-
-
-
-var nodemailer = require('nodemailer');
-require('dotenv').config();
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
-});
-
-var mailOptions = {
-  from: process.env.EMAIL,
-  to: input_email.join(','),
-  subject: 'Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
+    input_District.forEach(e => {
+        let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
+        delete data[0].date;
+        delete data[1].date;
+        if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
+            if(flag){
+                res.active = data[1].active-data[0].active;
+                res.confirmed = data[1].confirmed-data[0].confirmed;
+                res.deceased = data[1].deceased-data[0].deceased;
+                res.recovered = data[1].recovered-data[0].recovered;
+                flag=false;
+                sendEmail('DATA : '+e.district , JSON.stringify(res));
+            }
+        }else{
+            flag=true;
+            console.log('same');
+        }
+    });
+  })
+  .catch(function (error) {
     console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+  })
+  .finally(function () {
+  });
+},5000);
+
+function sendEmail(Title,Body){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+        }
+      });
+      var mailOptions = {
+        from: process.env.EMAIL,
+        to: input_email.join(','),
+        subject: Title,
+        text: Body
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
