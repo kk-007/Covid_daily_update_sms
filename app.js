82616b629aed80154bb4d40d6cb218b89b8ddb5c
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-06-11 16:35:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,40 +19,42 @@ let input_email = [
 
 let flag = true;
 console.log('started');
-setTimeout(()=>{
-    console.log('Execution in setInterval');
-    axios.get('https://api.covid19india.org/districts_daily.json')
-  .then(function (response) {
-    let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
-    let today = new Date().toISOString().slice(0,10);
-    let res={};
-
-    input_District.forEach(e => {
-        let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
-        delete data[0].date;
-        delete data[1].date;
-        if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
-            if(flag){
-                res.active = data[1].active-data[0].active;
-                res.confirmed = data[1].confirmed-data[0].confirmed;
-                res.deceased = data[1].deceased-data[0].deceased;
-                res.recovered = data[1].recovered-data[0].recovered;
-                flag=false;
-                sendEmail('DATA : '+e.district , JSON.stringify(res));
+while(true){
+    setTimeout(()=>{
+        console.log('Execution in setInterval');
+        axios.get('https://api.covid19india.org/districts_daily.json')
+      .then(function (response) {
+        let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
+        let today = new Date().toISOString().slice(0,10);
+        let res={};
+    
+        input_District.forEach(e => {
+            let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
+            delete data[0].date;
+            delete data[1].date;
+            if(!JSON.stringify(data[0])==JSON.stringify(data[1])){
+                if(flag){
+                    res.active = data[1].active-data[0].active;
+                    res.confirmed = data[1].confirmed-data[0].confirmed;
+                    res.deceased = data[1].deceased-data[0].deceased;
+                    res.recovered = data[1].recovered-data[0].recovered;
+                    flag=false;
+                    sendEmail('DATA : '+e.district , JSON.stringify(res));
+                }
+            }else{
+                flag=true;
+                sendEmail('Testing' ,'same');
+                console.log('same');
             }
-        }else{
-            flag=true;
-            sendEmail('Testing' ,'same');
-            console.log('same');
-        }
-    });
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-  });
-},5*1000);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      });
+    },5*1000);
+}
 
 function sendEmail(Title,Body){
     var transporter = nodemailer.createTransport({
