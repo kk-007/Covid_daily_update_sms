9d264cc843a122562245064dec07ff88bd3be002
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-06-12 22:41:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,29 +26,32 @@ function main(){
     setTimeout(()=>{
         console.log('Execution in setInterval');
         axios.get('https://api.covid19india.org/districts_daily.json')
-      .then(function (response) {
+      .then(async function (response) {
         let yesterday = new Date(Date.now() - 864e5).toISOString().slice(0,10);
         let today = new Date().toISOString().slice(0,10);
         let res={};
     
-        input_District.forEach(e => {
-          let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
-          delete data[0].date;
-          delete data[1].date;
-          if(JSON.stringify(data[0])!=JSON.stringify(data[1])){
-            if(flag){
-                res.active = data[1].active-data[0].active;
-                res.confirmed = data[1].confirmed-data[0].confirmed;
-                res.deceased = data[1].deceased-data[0].deceased;
-                res.recovered = data[1].recovered-data[0].recovered;
-                flag=false;
-                await sendEmail('DATA : '+e.district , JSON.stringify(res));
-            }
-          }else{
-              flag=true;
-              console.log('same');
-          }
-        });
+        await Promise.all(input_District.forEach(async e => {
+            await new Promise(async (resolve,reject)=>{
+              let data = response.data.districtsDaily[e.state][e.district].filter(e=>e.date===today || e.date===yesterday);
+              delete data[0].date;
+              delete data[1].date;
+              if(JSON.stringify(data[0])!=JSON.stringify(data[1])){
+                  if(flag){
+                      res.active = data[1].active-data[0].active;
+                      res.confirmed = data[1].confirmed-data[0].confirmed;
+                      res.deceased = data[1].deceased-data[0].deceased;
+                      res.recovered = data[1].recovered-data[0].recovered;
+                      flag=false;
+                      await sendEmail('DATA : '+e.district , JSON.stringify(res));
+                  }
+              }else{
+                  flag=true;
+                  console.log('same');
+              }
+              resolve(1);
+            })
+        }));
       })
       .catch(function (error) {
         console.log(error);
